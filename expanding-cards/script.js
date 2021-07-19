a4d5697b261102d749dc945a72baf469b000c6d9
JohnDoeMask <heyuehuang@hotmail.com>
2021-07-19 15:36:48 -0600

    notes 07/19

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,7 +1,10 @@
 const panels = document.querySelectorAll('.panel')
-
+/*
+  Add 'active' to clicked item
+*/
 panels.forEach(panel => {
     panel.addEventListener('click', () => {
+        // Remove 'active' class for all the items first
         removeActiveClasses()
         panel.classList.add('active')
     })
